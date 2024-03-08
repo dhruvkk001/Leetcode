d2c0a2ed6722ae8f63d9db9f2b126a3df3023b0f
Dhruv Kumar <77435632+dhruvkk001@users.noreply.github.com>
2024-03-08 15:47:37 +0530

    Time: 8 ms (31.49%) | Memory: 21.9 MB (17.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -15,3 +15,36 @@ public:
         return ans;
     }
 };
+// Sure, let's break down the simplified maxFrequencyElements method step by step:
+
+// Vector Initialization:
+// We initialize a vector frequency of size 101, where frequency[i] will store the count of the element i in the input vector a.
+// cpp
+// Copy code
+// vector<int> frequency(101, 0);
+// Calculating Maximum Frequency:
+// We iterate over each element in the input vector a.
+// For each element num, we increment the count of num in the frequency vector and update maxFreq if necessary.
+// cpp
+// Copy code
+// int maxFreq = 0;
+// for (int num : a) {
+//     maxFreq = max(maxFreq, ++frequency[num]);
+// }
+// Counting Elements with Maximum Frequency:
+// We iterate over the frequency vector to count how many elements have the maximum frequency maxFreq.
+// If the frequency of an element equals maxFreq, we add its frequency to the ans variable.
+// cpp
+// Copy code
+// int ans = 0;
+// for (int freq : frequency) {
+//     if (freq == maxFreq) {
+//         ans += freq;
+//     }
+// }
+// Return Result:
+// We return the total count of elements having the maximum frequency.
+// cpp
+// Copy code
+// return ans;
+// This simplified version combines the two loops for finding maxFreq and counting elements with maximum frequency into a single loop for efficiency and readability.
